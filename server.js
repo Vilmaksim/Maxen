0754a3151c9b22ad1b5762393b277ec7d6094838
Vilmaksim <vilmaks.maksim@gmail.com>
2026-08-02 15:55:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,17 +7,32 @@ const io = new Server(http);
 
 app.use(express.static("public"));
 
+let online = 0;
+
 io.on("connection", (socket) => {
+    online++;
+
     console.log("🟢 Пользователь подключился");
+
+    // отправляем всем количество людей
+    io.emit("online", online);
+
 
     socket.on("message", (data) => {
         io.emit("message", data);
     });
 
+
     socket.on("disconnect", () => {
+        online--;
+
         console.log("🔴 Пользователь вышел");
+
+        // обновляем количество
+        io.emit("online", online);
     });
 });
+
 
 const PORT = process.env.PORT || 3000;
 
