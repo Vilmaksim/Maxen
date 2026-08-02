e04288717c3eee7eff1f7f6ff7403b205614cb43
Vilmaksim <vilmaks.maksim@gmail.com>
2026-08-02 16:08:42 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,46 +1,53 @@
 const socket = io();
 
+
 const nicknameInput = document.getElementById("nickname");
 const messageInput = document.getElementById("message");
 const sendButton = document.getElementById("send");
 const chat = document.getElementById("chat");
 
+const onlineText = document.getElementById("online");
+const typingText = document.getElementById("typing");
 
-// Загружаем сохранённый ник
+
+// сохраняем ник
 nicknameInput.value = localStorage.getItem("nickname") || "";
 
-
-// Сохраняем ник
 nicknameInput.addEventListener("input", () => {
     localStorage.setItem("nickname", nicknameInput.value);
 });
 
 
-// Отправка сообщения
+// отправка сообщений
 sendButton.onclick = () => {
 
     const nickname = nicknameInput.value.trim();
     const text = messageInput.value.trim();
+
 
     if (!nickname) {
         alert("Введите ник!");
         return;
     }
 
+
     if (!text) {
         return;
     }
+
 
     socket.emit("message", {
         nickname: nickname,
         text: text
     });
 
+
     messageInput.value = "";
 };
 
 
-// Получение сообщения
+
+// получение сообщений
 socket.on("message", (data) => {
 
     chat.innerHTML += `
@@ -50,10 +57,55 @@ socket.on("message", (data) => {
     `;
 
     chat.scrollTop = chat.scrollHeight;
+
 });
 
-const onlineText = document.getElementById("online");
 
+
+// онлайн
 socket.on("online", (count) => {
+
     onlineText.innerHTML = `🟢 Онлайн: ${count}`;
+
+});
+
+
+
+// печатает...
+let typingTimer;
+
+
+messageInput.addEventListener("input", () => {
+
+    const nickname = nicknameInput.value || "Гость";
+
+
+    socket.emit("typing", nickname);
+
+
+    clearTimeout(typingTimer);
+
+
+    typingTimer = setTimeout(() => {
+
+        socket.emit("stopTyping");
+
+    }, 1000);
+
+});
+
+
+
+socket.on("typing", (nickname) => {
+
+    typingText.innerHTML = `${nickname} печатает...`;
+
+});
+
+
+
+socket.on("stopTyping", () => {
+
+    typingText.innerHTML = "";
+
 });
