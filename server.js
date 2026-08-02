33f9efab24c34c04f671f1484ec52182e9d701d0
Vilmaksim <vilmaks.maksim@gmail.com>
2026-08-02 15:47:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.static("public"));
 io.on("connection", (socket) => {
     console.log("🟢 Пользователь подключился");
 
-    socket.on("message", (msg) => {
-        io.emit("message", msg);
+    socket.on("message", (data) => {
+        io.emit("message", data);
     });
 
     socket.on("disconnect", () => {
@@ -19,6 +19,8 @@ io.on("connection", (socket) => {
     });
 });
 
-http.listen(3000, "0.0.0.0", () => {
-    console.log("🚀 Maxen запущен: http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Maxen запущен на порту ${PORT}`);
 });
